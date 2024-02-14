3c62e73285f2ad8acf96a82d754dc6e952a3d0ef
Emil <emilada@chalmers.se>
2024-02-14 13:15:17 +0100

    Co-authored-by: MoaLilja <MoaLilja@users.noreply.github.com>

--- server/src/service/profile.ts
+++ server/src/service/profile.ts
@@ -1,10 +1,21 @@
 import { Profile } from "../model/profile.interface";
+import path from "path";
+import jwt from "jsonwebtoken";
+
+
+const publicPath = path.resolve(__dirname, '../../../client/public');
+const dbPath = path.resolve(__dirname,'../../DB/users.json');
+
+interface User {
+    username: string;
+    password: string;
+}
 
 export class ProfileService {
+   
 
-    
-    async login() : Promise<> {
-        return ;
-    } 
+
 
 }
+
+
