3ac0672591fbba206846ce63a421b8c28b89fc37
234686 <miltonniklasson@gmail.com>
2024-02-14 10:18:26 +0100

    react-mockup

--- server/src/start.ts
+++ server/src/start.ts
@@ -5,6 +5,7 @@ import path from "path";
 import { mainRouter } from "./router/main";
 import { profileRouter } from "./router/profile";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 
 // import {  } from "./router/...";
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.static(publicPath));
 app.use(cookieParser());
 app.use(bodyParser());
+app.use(cors());
 
 
 app.use("/", mainRouter);
