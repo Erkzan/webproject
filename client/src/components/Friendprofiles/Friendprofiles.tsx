378406514f65da4e86987bcd9fa9ced6b3203648
moafalkenberg <moalilj@student.chalmers.se>
2024-02-26 15:46:53 +0100

    design

---
+++ client/src/components/Friendprofiles/Friendprofiles.tsx
@@ -0,0 +1,16 @@
+import "./Friendprofiles.css";
+
+function Friend() {
+    return (
+      <>
+          <div className="Friendprofile-container">
+            <div className="profile-pic"></div>
+            <div className="user">Erik Karlsson</div>
+            <div className="bio">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum culpa facere minus dicta quasi repudiandae ipsam? Dolor, quis animi dolorem, deserunt quos repudiandae temporibus ipsa, atque dicta obcaecati deleniti esse?</div>
+          </div>
+      </>
+    );
+  }
+  
+  export default Friend;
+  
--- client/src/pages/Friends.tsx
+++ client/src/pages/Friends.tsx
@@ -1,11 +1,30 @@
 import Friend from "../components/Friend/Friend";
 import NavBar from "../components/Navbar/Navbar";
 import Post from "../components/Post/Post";
+import Friendprofiles from "../components/Friendprofiles/Friendprofiles";
+import "./Friends.css";
 
 const Friends = () => {
     return (
         <>
             <NavBar/>
+            <header></header>
+            <div className="page">
+                <div className="row">
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                    <Friendprofiles/>
+                </div>
+            </div>
         </>
     );
 };
