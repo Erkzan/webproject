fffb35e2b325ed1c67d72d6bd54b1d986037c958
Erik Karlsson <erik.ekekullen@gmail.com>
2024-03-12 20:13:03 +0100

    blabla

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,35 +1,37 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar/Navbar";
 import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 import Profile from "../components/Profile/Profile";
 
 import classes from "./Search.module.css";
 
-async function getSearch(search:String) {
-  let response = await fetch("http://localhost:8080/profile/getSearchedProfiles", {
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ search }),
-  });
-  
+async function getSearch(search: String) {
+  let response = await fetch(
+    "http://localhost:8080/profile/getSearchedProfiles",
+    {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ search }),
+    }
+  );
+
   let txtResponse = await response.text();
 
   console.log(txtResponse);
   // Check if txtResponse is not empty
   if (!txtResponse) {
-    console.error('Response from getPostById is empty');
+    console.error("Response from getPostById is empty");
     return null; // Optionally, return a default value or handle the error as needed
   }
 
-  
   let profiles = JSON.parse(txtResponse);
-    
+
   console.log(profiles);
-  return profiles;  //an array of profiles
+  return profiles; //an array of profiles
 }
 
 const Search = () => {
@@ -61,12 +63,9 @@ const Search = () => {
 
   return (
     <>
-    <title>Search</title>
+      <title>Search</title>
       <Navbar />
-      
-
       <div className={classes.usersContainer}>{foundProfiles}</div>
-      <div className={classes.postsContainer}></div>
     </>
   );
 };
